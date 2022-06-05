3a2de542776055ca055a67171ba40dd11452220a
Jam C <jamaicacavalida@gmail.com>
2022-06-05 23:34:33 +0800

    Add project files.

---
+++ eGamersShop/Controllers/HomeController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace eGamersShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        string connDB = WebConfigurationManager.ConnectionStrings["connDB"].ConnectionString;
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ProductEntry()
+        {
+            return View();
+        }
+
+        public ActionResult ListAllProducts()
+        {
+            return View();
+        }
+
+        public ActionResult EntryProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProductEntry(FormCollection collection, HttpPostedFileBase uploadImg)
+        {
+            var itmcde = Convert.ToString(collection["txtCode"]);
+            var itmname = Request["txtname"];
+            var itmdesc = Request["txtDesc"];
+            var itmprce = Request["txtprice"];
+            var itmonhand = Request["txtonhand"];
+            var date = Request["datepicker"];
+
+            if (uploadImg != null)
+            {
+                try
+                {
+                    string filename = Path.GetFileName(uploadImg.FileName);
+                    var checkextension = Path.GetExtension(uploadImg.FileName).ToUpper();
+                    int filesize = uploadImg.ContentLength;
+                    string logPath = "C:\\Uploads";
+                    string filepath = Path.Combine(logPath, filename);
+                    uploadImg.SaveAs(filepath);
+                    using (var db = new SqlConnection(connDB))
+                    {
+                        db.Open();
+                        using (var cmd = db.CreateCommand())
+                        {
+                            cmd.CommandType = CommandType.Text;
+                            cmd.CommandText = "INSERT INTO ITMTBL (ITMNUM,ITMNAME,ITMDESC,ITMIMG,ITMONHAND,ITMPRICE,ITMDATE)"
+                                + " VALUES ("
+                                + " @NUM,"
+                                + " @NAME,"
+                                + " @DESC,"
+                                + " @IMG,"
+                                + " @ONHAND,"
+                                + " @PRICE,"
+                                + " @DATE)";
+                            cmd.Parameters.AddWithValue("@NUM", itmcde);
+                            cmd.Parameters.AddWithValue("@NAME", itmname);
+                            cmd.Parameters.AddWithValue("@DESC", itmdesc);
+                            cmd.Parameters.AddWithValue("@IMG", filename);
+                            cmd.Parameters.AddWithValue("@ONHAND", itmonhand);
+                            cmd.Parameters.AddWithValue("@PRICE", itmprce);
+                            cmd.Parameters.AddWithValue("@DATE", date);
+                            var ctr = cmd.ExecuteNonQuery();
+                            if (ctr >= 1)
+                            {
+                                Response.Write("<script>alert('Data Save')</script>");
+                            }
+                            else
+                                Response.Write("<script>alert('Data Not Save')</script>");
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('Something went wrong...')</script>");
+                    Response.Write(ex);
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult getItemCode()
+        {
+            var data = new List<object>();
+            var itmcode = "";
+            using (var db = new SqlConnection(connDB))
+            {
+                db.Open();
+                using (var cmd = db.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT MAX(ID) as 'MAXID' FROM ITMTBL";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        data.Add(new
+                        {
+                            itmcode = reader["MAXID"].ToString(),
+                        });
+                    }
+                }
+            }
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ eGamersShop/Scripts/myScripts/myScript.js
@@ -0,0 +1,35 @@
+﻿
+function getImagePreview() {
+    var txt = (event.target.files[0].name).split(".").pop().toLowerCase();
+    if (txt == "jpg" || txt == "png" || txt == "gif" || txt == "bmp") {
+        var image = URL.createObjectURL(event.target.files[0]);
+        var imagediv = document.getElementById("preview");
+        var newimg = document.createElement('img');
+        newimg.src = image;
+        newimg.width = "200";
+        imagediv.appendChild(newimg);
+    } else {
+        alert("File uploaded is invalid");
+        $("#preview").html("");
+        $("#uploadImg").val("");
+    }
+}
+
+$(function () {
+    var numCode;
+    ///
+    $.post("../home/getItemCode", {
+        //this is part when you are going to send data to the server.
+    }, function (res) {
+        var num = parseInt(res[0].itmcode) + 1;
+        numCode = $("#txtCode").val("I000000000" + num);
+        $("#txtCode").prop('disabled', true);
+    });
+    ///
+    $("#txtCode").val(numCode);
+    $("#productRecord").submit(function () {
+        $("#txtCode").prop('disabled', false);
+    });
+
+
+});
